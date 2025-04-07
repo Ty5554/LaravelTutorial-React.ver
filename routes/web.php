0f2746640b220dcb1d1eb96ca72773f0b41ef950
Yo Tanaka <y.tanaka@hybrid-technologies.co.jp>
2025-04-07 14:50:05 +0900

    Inertiaを追加

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,12 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/', function () {
+    return Inertia::render('Home', [
+    'user' => auth()->user() ?? ['name' => 'ゲスト'],
+    ]);
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
